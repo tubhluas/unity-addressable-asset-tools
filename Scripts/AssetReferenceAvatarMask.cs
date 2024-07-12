41d2dd8df3e5b7a735d05eac5e4063f32c7a9784
insthync <ittipon.bay@gmail.com>
2024-07-12 18:13:39 +0700

    feat: add asset reference avatar mask

---
+++ Scripts/AssetReferenceAvatarMask.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Insthync.AddressableAssetTools
+{
+    [System.Serializable]
+    public class AssetReferenceAvatarMask : AssetReference
+    {
+#if UNITY_EDITOR
+        public AssetReferenceAvatarMask(AvatarMask scene) : base(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(scene)))
+        {
+        }
+
+        public override bool ValidateAsset(string path)
+        {
+            return ValidateAsset(AssetDatabase.LoadAssetAtPath<AvatarMask>(path));
+        }
+
+        public override bool ValidateAsset(Object obj)
+        {
+            return (obj != null) && (obj is AvatarMask);
+        }
+
+        public override bool SetEditorAsset(Object value)
+        {
+            if (!base.SetEditorAsset(value))
+            {
+                return false;
+            }
+            return value is AvatarMask;
+        }
+#endif
+    }
+}
