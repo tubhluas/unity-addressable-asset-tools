2fbb1e7959b326a799891edff8ff0cf0a71da3e8
insthync <ittipon.bay@gmail.com>
2024-07-12 15:27:15 +0700

    feat: add asset reference audio clip

---
+++ Scripts/AssetReferenceAudioClip.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace Insthync.AddressableAssetTools
+{
+    [System.Serializable]
+    public class AssetReferenceAudioClip : AssetReference
+    {
+#if UNITY_EDITOR
+        public AssetReferenceAudioClip(AudioClip scene) : base(AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(scene)))
+        {
+        }
+
+        public override bool ValidateAsset(string path)
+        {
+            return ValidateAsset(AssetDatabase.LoadAssetAtPath<AudioClip>(path));
+        }
+
+        public override bool ValidateAsset(Object obj)
+        {
+            return (obj != null) && (obj is AudioClip);
+        }
+
+        public override bool SetEditorAsset(Object value)
+        {
+            if (!base.SetEditorAsset(value))
+            {
+                return false;
+            }
+            return value is AudioClip;
+        }
+#endif
+    }
+}
